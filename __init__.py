972c92155e47dfe1192f8aec5331329c9a89e76b
Kessler Dev <kessler.dev@gmail.com>
2025-10-01 10:23:17 +0200

    Add env var node

--- __init__.py
+++ __init__.py
@@ -1,11 +1,14 @@
 from .text.multiline_choice import MultilineTextChoice
+from .text.env import EnvironmentVariableGetter
 
 NODE_CLASS_MAPPINGS = {
-    "KesslerDev_MultilineTextChoice": MultilineTextChoice
+    "KesslerDev_MultilineTextChoice": MultilineTextChoice,
+    "KesslerDev_EnvironmentVariableGetter": EnvironmentVariableGetter
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "KesslerDev_MultilineTextChoice": "Multiline Text Choice (Kesslerdev)"
+    "KesslerDev_MultilineTextChoice": "Multiline Text Choice (Kesslerdev)",
+    "KesslerDev_EnvironmentVariableGetter": "Environment Variable Getter (Kesslerdev)"
 }
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
---
+++ text/env.py
@@ -0,0 +1,22 @@
+class EnvironmentVariableGetter:
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "var_name": ("STRING", {"default": "", "tooltip": "Name of the environment variable to get."}),
+            },
+        }
+
+    DESCRIPTION = "Get the value of an environment variable."
+    RETURN_TYPES = ("STRING",)
+    # Names for the returned values
+    RETURN_NAMES = ("value",)
+    # Function to process the inputs
+    FUNCTION = "process"
+    # Category under which this node will appear in the UI
+    CATEGORY = "Utility"
+
+    def process(self, var_name):
+        import os
+        selected_text = os.getenv(var_name, "")
+        return (selected_text,)
